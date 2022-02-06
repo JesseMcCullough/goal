6d20e7f7fe634a97429bccb7e5e5d868bd489769
Jesse McCullough <jesse@jessem.dev>
2022-02-06 01:58:05 -0500

    Added a link to each goal

--- includes/goal/view-goal.php
+++ includes/goal/view-goal.php
@@ -13,15 +13,17 @@ $category = $goal->getCategory();
 
 ?>
 
-<div class="goal<?php if (isset($_GET["goalId"])) { echo " view-goal"; } else { echo " click"; } ?>" style="border-color: <?php echo $category->getHexColor(); ?>"
-    data-goal-id="<?php echo $goalId; ?>"
-    data-category-id="<?php echo $category->getId(); ?>"
-    data-category-hex-color="#<?php echo $category->getHexColor(); ?>">
-    <span class="name"><?php echo $goal->getName(); ?></span>
-    <span class="date"><img src="images/clock-icon.png" /><?php echo $goal->getDateFormatted(); ?></span>
-    <div class="progress">
-        <?php $progressPercentage = $goal->getProgressPercentage(); ?>
-        <div class="completion-bar" style="width: <?php echo $progressPercentage; ?>"></div>
-        <span class="percent"><?php echo $progressPercentage; ?></span>
+<a href="view-goal.php?goalId=<?php echo $goalId; ?>" class="goal-link">
+    <div class="goal<?php if (isset($_GET["goalId"])) { echo " view-goal"; } else { echo " click"; } ?>" style="border-color: <?php echo $category->getHexColor(); ?>"
+        data-goal-id="<?php echo $goalId; ?>"
+        data-category-id="<?php echo $category->getId(); ?>"
+        data-category-hex-color="#<?php echo $category->getHexColor(); ?>">
+        <span class="name"><?php echo $goal->getName(); ?></span>
+        <span class="date"><img src="images/clock-icon.png" /><?php echo $goal->getDateFormatted(); ?></span>
+        <div class="progress">
+            <?php $progressPercentage = $goal->getProgressPercentage(); ?>
+            <div class="completion-bar" style="width: <?php echo $progressPercentage; ?>"></div>
+            <span class="percent"><?php echo $progressPercentage; ?></span>
+        </div>
     </div>
-</div>
+</a>
